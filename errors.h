b9cb05ae70e5af4d339d3afc604ce8faf82721e9
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2024-11-28 15:20:21 -0500

    Custom error

---
+++ errors.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <exception>
+
+namespace myvc {
+
+class not_found : public std::exception {
+    std::string resource;
+
+public:
+    not_found(std::string resource) : resource {std::move(resource)} {}
+
+    const char *what() const noexcept {
+        return resource.c_str();
+    }
+};
+
+}
